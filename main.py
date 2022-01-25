8a543cd3fbf621e13a332ae178e957acbef77afe
Farkas Ükös <ukosfarkas@gmail.com>
2022-01-25 11:01:55 +0100

    Nem értem a pythonos classokat.

---
+++ main.py
@@ -0,0 +1,10 @@
+class Osztaly:
+    def __init__(self, *args, **kwargs):
+        
+        tanulokod = 
+
+
+
+with open('input.txt', 'r', encoding='utf8') as f:
+    for sor in f:
+        lista.append(sor)
