6db472632987505c9d9f198c99d35de82c3aa629
Farkas Ükös <ukosfarkas@gmail.com>
2022-01-27 13:55:15 +0100

    of

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-class Osztaly:
-    def __init__(self, *args, **kwargs):
-        
-        tanulokod = 
+class Tanulo:
+    lista = []
+    def __init__(self, tanulokod, nev, micsop, acsop, mnyelv, nem, egyuttlakok, testverszam):
+        self.tanulokod = tanulokod
+        self.nev = nev
+        self.micsop = micsop
+        self.acsop = acsop
+        self.mnyelv = mnyelv
+        self.nem = nem
+        self.egyuttlakok = egyuttlakok
+        self.testverszama = testverszam
+        Tanulo.lista.append(self)
 
+with open("input.txt", "r", encoding="utf8") as f:
 
-
-with open('input.txt', 'r', encoding='utf8') as f:
     for sor in f:
-        lista.append(sor)
+        s = sor.strip().split(";")
+        t = Tanulo(int(s[0]), s[1], s[2], s[3], s[4], s[5], int(s[6]), int(s[7]))
+
+
+for t in Tanulo.lista:
+    print(t.nev)
+
+print(len(Tanulo.lista))
